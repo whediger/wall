dd868280a8c186910218e899e26ba8dbb4b57e9c
Wes Hediger <wes.hediger@gmail.com>
2016-06-17 12:59:53 -0600

    add new_user.hbs, create_post.hbs

--- wall/routes/index.js
+++ wall/routes/index.js
@@ -4,7 +4,15 @@ var knex = require('../db/knex');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index');
+});
+
+router.get('/create_post', function(req, res, next){
+    res.render('create_post');
+});
+
+router.get('/new_user', function(req, res, next){
+    res.render('new_user');
 });
 
 module.exports = router;
